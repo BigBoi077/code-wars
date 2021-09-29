9938a593864d9dea1981c5185e190d74c599578a
Tommy-Lee Pigeon <tom_pig9@hotmail.com>
2021-09-29 14:16:25 -0400

    [CW-008] Added missing team_name to the list

--- app/Models/Brokers/StudentBroker.php
+++ app/Models/Brokers/StudentBroker.php
@@ -18,9 +18,10 @@ class StudentBroker extends Broker
 
     public function getAll()
     {
-        $sql = "SELECT s.da, s.team_id, s.cash, p.firstname, p.lastname from codewars.student s 
+        $sql = "SELECT s.da, s.team_id, s.cash, p.firstname, p.lastname, t.name as team_name from codewars.student s 
                 join codewars.user u on s.da = u.da
                 join codewars.person p on u.da = p.da
+				join codewars.team t on s.team_id = t.id
                 ORDER BY s.da";
         return $this->select($sql);
     }
