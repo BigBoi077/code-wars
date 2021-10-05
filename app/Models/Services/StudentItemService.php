c189a702d07506701226ac05efd87e0467ed6496
Julien Deguire <julien.deguire@gmail.com>
2021-10-05 13:58:29 -0400

    [CW-002] Achat d'articles (ca fonctionnait de mon cote, mais jai pas encore fait bcp de tests. Aussi jai pas tester si ca fonctionnait pour le professeur)

---
+++ app/Models/Services/StudentItemService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Models\Services;
+
+use Models\Brokers\StudentItemBroker;
+
+class StudentItemService
+{
+
+
+    public static function getAllByDa($da)
+    {
+        return (new StudentItemBroker())->getAllWithDa($da);
+    }
+
+    public static function exists($item_id, $da): bool
+    {
+        return (new StudentItemBroker())->getWithIdAndDa($item_id, $da) != null;
+    }
+
+    public static function create($item_id, $da)
+    {
+        (new StudentItemBroker())->insert($item_id, $da);
+    }
+}
