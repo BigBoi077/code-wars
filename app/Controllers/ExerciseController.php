0e15be9339f3b0cbf050704e5a3efa4fa012ace5
Joshua Leblanc <leblancjoshua077@gmail.com>
2021-10-12 18:23:17 -0400

    Ayo jé its up to you

--- app/Controllers/ExerciseController.php
+++ app/Controllers/ExerciseController.php
@@ -3,15 +3,14 @@
 namespace Controllers;
 
 use Models\Services\ExerciseService;
-use Zephyrus\Network\Response;
 
 class ExerciseController extends Controller
 {
-
     public function initializeRoutes()
     {
         $this->get('/exercises', 'exercises');
         $this->get('/exercises/{id}', 'exerciseDetail');
+        $this->post('/submit/exercise', 'exerciseUpload');
     }
 
     public function exercises()
@@ -21,10 +20,20 @@ class ExerciseController extends Controller
         ]);
     }
 
-    public function exerciseDetail($id): Response
+    public function exerciseDetail($id)
     {
         return $this->render('exercises/exercise_details', [
             'exercise' => ExerciseService::get($id)
         ]);
+    }
+
+    public function exerciseUpload()
+    {
+        echo "yo";
+        $form = $this->buildForm();
+        echo "yo";
+        var_dump($this->request->getFile("exercise"));
+        var_dump($form->buildObject());
+        die();
     }
 }
